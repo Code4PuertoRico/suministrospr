bf1a3d1c97cdc3ecd2e592325b84d991d11ebc8a
José Padilla <jpadilla@webapplicate.com>
2020-01-13 18:26:09 -0500

    Add municipalities

--- suministrospr/suministros/models.py
+++ suministrospr/suministros/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from .constants import MUNICIPALITIES
 from ..utils.models import BaseModel
-
-MUNICIPALITY_CHOICES = []
 
 
 class Suministro(BaseModel):
+    MUNICIPALITY_CHOICES = [(value.lower(), value) for value in MUNICIPALITIES]
+
     title = models.CharField(max_length=255)
     municipality = models.CharField(max_length=255, choices=MUNICIPALITY_CHOICES)
     content = RichTextField()
