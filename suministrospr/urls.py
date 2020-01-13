50da3ee894393552fcc84f94cc4f9d45fba0ad3f
Froilan Irizarry <froi@github.com>
2020-01-13 17:58:57 -0400

    Fixed folder issue - moved suministrospr/suministrospr to suministorspr

---
+++ suministrospr/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('trix/', include('trix.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
