d7433d546419178ae14e42faa8c555c1e29a9026
Froilan Irizarry <froi@github.com>
2020-01-13 17:48:04 -0400

    Fixed path typo

--- suministrospr/suministrospr/urls.py
+++ suministrospr/suministrospr/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('trix/'. include('trix.urls')),
+    path('trix/', include('trix.urls')),
 ]
 
 if settings.DEBUG:
