ebbef11edc8630f7477bb0d0a7db1bfa678c5d14
José Padilla <jpadilla@webapplicate.com>
2020-01-14 02:13:29 -0500

    Improve styling for home page

---
+++ suministrospr/suministros/templatetags/suministro_extras.py
@@ -0,0 +1,10 @@
+from django import template
+from django.templatetags.static import static
+from django.utils.text import slugify
+
+register = template.Library()
+
+
+@register.simple_tag
+def escudo_static(value):
+    return static(f"img/escudos/{slugify(value)}.jpg")
