9b6ce29991b6c4bc64c2a9c1d4cb07cba87e0d47
Froilan Irizarry <froi@github.com>
2020-01-13 23:45:30 -0400

    Bug fixes - Changed model and constant import to be relatice import - Fixed typo in add_argument call - Fixed error in add_argument call. Param is now a string - Fixed error in how we were getting data from data dictionary

--- suministrospr/suministros/management/commands/import_data.py
+++ suministrospr/suministros/management/commands/import_data.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
-from suministros.models import Suministro
+from ...models import Suministro
 from os import path
 import glob
 import json
-from suministros.constants import MUNICIPALITIES
+from ...constants import MUNICIPALITIES
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_arguments(target_folder, type=str)
+        parser.add_argument('target_folder', type=str)
 
     def handle(self, *args, **options):
         target_folder = options['target_folder']
@@ -21,14 +21,14 @@ class Command(BaseCommand):
                     data = json.load(json_file)
                     for item in data:
                         suministro = Suministro()
-                        suministro.content = item.content
-                        suministro.title = item.title
-                        if item.municipio in MUNICIPALITIES:
-                            suministro.municipality = item.municipio
+                        suministro.content = item['content']
+                        suministro.title = item['title']
+                        if item.get('municipio', None) in MUNICIPALITIES:
+                            suministro.municipality = item['municipio']
                             suministro.save()
                         else :
-                            self.stderr.write(f'Unrecognized municipality {item.municipio} in file {file_path}')
-                            suministro.municipality = 'N/A'
+                            self.stderr.write(f'Unrecognized municipality {item["municipio"]} in file {file_path}')
+                            suministro = None
             except EnvironmentError:
                 self.stderr.write(f'There was an error reading file {file_path}')
 
