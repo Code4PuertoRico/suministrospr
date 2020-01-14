be04c220f022805b8133cdf2ee44b1812cdef97c
José Padilla <jpadilla@webapplicate.com>
2020-01-14 17:16:51 -0500

    Setup versioning for Suministro model (#41)

--- suministrospr/suministros/admin.py
+++ suministrospr/suministros/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+
+from reversion.admin import VersionAdmin
+
 from .models import Suministro
 
 
 @admin.register(Suministro)
-class SuministroAdmin(admin.ModelAdmin):
+class SuministroAdmin(VersionAdmin):
     list_display = ["title", "slug", "municipality", "created_at", "modified_at"]
