4e864433cf5d441e25e5d8cef97ec49ebd602d95
José Padilla <jpadilla@webapplicate.com>
2020-01-13 19:32:23 -0500

    Add slugs

--- suministrospr/suministros/models.py
+++ suministrospr/suministros/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+
 from ckeditor.fields import RichTextField
+from django_extensions.db.fields import AutoSlugField
+
 from .constants import MUNICIPALITIES
 from ..utils.models import BaseModel
 
@@ -8,6 +11,7 @@ class Suministro(BaseModel):
     MUNICIPALITY_CHOICES = [(value.lower(), value) for value in MUNICIPALITIES]
 
     title = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from=["title", "municipality"])
     municipality = models.CharField(max_length=255, choices=MUNICIPALITY_CHOICES)
     content = RichTextField()
 
