92741bd10204202ffb78026669787bbc6835c47b
Froilan Irizarry <froi@github.com>
2020-01-13 23:09:41 -0400

    Initial commit import command

---
+++ suministrospr/suministros/management/commands/import_data.py
@@ -0,0 +1,6 @@
+from django.core.management.base import BaseCommand, CommandError
+from suministros.models import Suministro
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
