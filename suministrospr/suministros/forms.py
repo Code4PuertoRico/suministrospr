c57f25e5a21ccca7c4a9cf8b786a47c69e0038e7
José Padilla <jpadilla@webapplicate.com>
2020-01-13 23:36:20 -0500

    Sanitize content when saving form
    This will strip anything that's not explicitely allowed by `bleach`.

--- suministrospr/suministros/forms.py
+++ suministrospr/suministros/forms.py
@@ -1,9 +1,39 @@
+import bleach
 from django import forms
 
 from .models import Suministro
+
+
+ALLOWED_TAGS = [
+    "div",
+    "address",
+    "a",
+    "ul",
+    "ol",
+    "li",
+    "h1",
+    "h2",
+    "h3",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "p",
+    "strong",
+    "em",
+    "u",
+    "s",
+    "br",
+    "hr",
+    "pre",
+]
 
 
 class SuministroModelForm(forms.ModelForm):
     class Meta:
         model = Suministro
         fields = ["title", "municipality", "content"]
+
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+        return bleach.clean(content, tags=ALLOWED_TAGS, strip=True,)
