98a3d9d0b59482551f98d09b98e285dba907e3a5
Giovanni Collazo <hello@gcollazo.com>
2020-01-15 13:42:00 -0400

    Add dummy cache backend on development

--- suministrospr/settings.py
+++ suministrospr/settings.py
@@ -173,6 +173,10 @@ class Development(Common):
 
     MIDDLEWARE = Common.MIDDLEWARE + ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
+    CACHES = {
+        "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}
+    }
+
 
 class Production(Common):
     """
