f488cb112db0a0e8dffa39698bbb75d49d284f79
José Padilla <jpadilla@webapplicate.com>
2020-01-15 17:44:29 -0500

    Always add debug toolbar urls

--- suministrospr/urls.py
+++ suministrospr/urls.py
@@ -1,14 +1,9 @@
-from django.conf import settings
+import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     path("", include("suministrospr.suministros.urls")),
 ]
-
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
