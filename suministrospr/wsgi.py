f3053af6f0bee5a8db54952599cedbca10767f6f
José Padilla <jpadilla@webapplicate.com>
2020-01-15 17:13:41 -0500

    Fix sort order issue

--- suministrospr/wsgi.py
+++ suministrospr/wsgi.py
@@ -6,11 +6,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 import os
 
-from configurations.wsgi import get_wsgi_application
-
 configuration = os.getenv("ENVIRONMENT", "development").title()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suministrospr.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", configuration)
 
+from configurations.wsgi import get_wsgi_application  # noqa isort:skip
 
 application = get_wsgi_application()
