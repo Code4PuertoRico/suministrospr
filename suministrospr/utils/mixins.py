000ae80ecb1a6974467d665cc1acb125a0085eaf
José Padilla <jpadilla@webapplicate.com>
2020-01-14 22:09:56 -0500

    Make sure callback doesnt return boolean

--- suministrospr/utils/mixins.py
+++ suministrospr/utils/mixins.py
@@ -9,6 +9,10 @@ class CacheMixin:
     def get_cache_key(self):
         return self.cache_key
 
+    def _cache_rendered_response(self, response):
+        cache_key = self.get_cache_key()
+        cache.set(cache_key, response, self.cache_timeout)
+
     def dispatch(self, *args, **kwargs):
         cache_key = self.get_cache_key()
 
@@ -21,8 +25,6 @@ class CacheMixin:
         response = super().dispatch(*args, **kwargs)
 
         if cache_key:
-            response.add_post_render_callback(
-                lambda r: cache.set(cache_key, r, self.cache_timeout)
-            )
+            response.add_post_render_callback(self._cache_rendered_response)
 
         return response
