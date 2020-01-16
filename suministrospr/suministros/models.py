423f04f4bd0e29461f40813dff854a560edcf67d
Raúl Negrón <raul.esteban.negron@gmail.com>
2020-01-16 15:36:56 -0400

    Remove old municipality field

--- suministrospr/suministros/models.py
+++ suministrospr/suministros/models.py
@@ -33,13 +33,10 @@ class Municipality(BaseModel):
 
 
 class Suministro(BaseModel):
-    MUNICIPALITY_CHOICES = [(value, label) for value, label in MUNICIPALITIES.items()]
-
     title = models.CharField(max_length=255)
     slug = AutoSlugField(
         populate_from=["title", "municipality"], overwrite_on_add=False, max_length=255
     )
-    municipality = models.CharField(max_length=255, choices=MUNICIPALITY_CHOICES)
 
     municipality_fk = models.ForeignKey(Municipality, null=True, default=None, on_delete=models.SET_NULL)
 
