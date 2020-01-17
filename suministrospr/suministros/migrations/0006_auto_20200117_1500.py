87af19c2c06503a3661953a742cdde388a8e1619
José Padilla <jpadilla@webapplicate.com>
2020-01-17 14:14:56 -0500

    Correctly fix dependency on taggit for migration

--- suministrospr/suministros/migrations/0006_auto_20200117_1500.py
+++ suministrospr/suministros/migrations/0006_auto_20200117_1500.py
@@ -8,26 +8,24 @@ import suministrospr.utils.fields
 
 
 def migrate_taggit_items(apps, schema_editor):
-    try:
-        Taggit = apps.get_model("taggit", "Tag")
-        TaggedItem = apps.get_model("taggit", "TaggedItem")
-        Tag = apps.get_model("suministros", "Tag")
-        Suministro = apps.get_model("suministros", "Suministro")
+    Taggit = apps.get_model("taggit", "Tag")
+    TaggedItem = apps.get_model("taggit", "TaggedItem")
+    Tag = apps.get_model("suministros", "Tag")
+    Suministro = apps.get_model("suministros", "Suministro")
 
-        for tag in Taggit.objects.all():
-            created_tag = Tag.objects.create(name=tag.name)
+    for tag in Taggit.objects.all():
+        created_tag = Tag.objects.create(name=tag.name)
 
-            for item in TaggedItem.objects.filter(tag=tag):
-                suministro = Suministro.objects.get(pk=item.object_id)
-                suministro.tags.add(created_tag)
-    except Exception:
-        pass
+        for item in TaggedItem.objects.filter(tag=tag):
+            suministro = Suministro.objects.get(pk=item.object_id)
+            suministro.tags.add(created_tag)
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ("suministros", "0005_auto_20200115_2301"),
+        ("taggit", "0003_taggeditem_add_unique_index"),
     ]
 
     operations = [
