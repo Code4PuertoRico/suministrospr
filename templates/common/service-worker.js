e014fc4d414aad6ee4165b6184207358db249bb7
Carlos Ramirez <carlosarg54@gmail.com>
2020-02-15 14:10:40 -0600

    Update service worker to use online first approach

--- templates/common/service-worker.js
+++ templates/common/service-worker.js
@@ -1,35 +1,77 @@
-let cacheName = "suministrospr-cache";
-let dynamicCacheName = "dynamic-cache";
+const version = "0.1.0";
+const cacheName = `suministrospr-cache-${version}`;
 // For now only root and offline since it will dynamically add the other urls
 let filesToCache = ["/", "/offline/"];
 
+/**
+ * Deletes all caches except the ones in cacheName
+ */
+const deleteCaches = () => {
+  return caches
+    .keys()
+    .then(keys => keys.filter(key => key !== cacheName))
+    .then(keys => keys.map(key => caches.delete(key)))
+    .then(delArray => Promise.all(delArray));
+};
+
+/**
+ * Add response to cache
+ * @param {Request} request
+ * @param {Response} response
+ */
+const cacheResponse = (request, response) => {
+  const clonedResponse = response.clone();
+  caches.open(cacheName).then(cache => {
+    cache.put(request, clonedResponse);
+  });
+  return response;
+};
+
+/**
+ * Fetch and update cache
+ * @param {Request} request
+ */
+const fetchAndCache = request => {
+  return fetch(request)
+    .then(response => {
+      return cacheResponse(request, response);
+    })
+    .catch(err => console.log(err));
+};
+
+/**
+ * Check if response is valid
+ * @param {Response} response
+ */
+const isValidResponse = response => {
+  return response && (response.ok || response.type === "opaque");
+};
+
+/**
+ * Delete previous caches if any and claim
+ */
+self.addEventListener("activate", event => {
+  deleteCaches();
+  return event.waitUntil(clients.claim());
+});
+
 self.addEventListener("install", event => {
-  event.waitUntil(
+  return event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(filesToCache);
-    })
+    }).then(skipWaiting())
   );
 });
 
-// If request is not in cache fetch the url and dynamically add the response to the cache
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(response => {
-          const clonedResponse = response.clone();
-          caches.open(dynamicCacheName).then(cache => {
-            cache.put(event.request.url, clonedResponse);
-          });
-          return response;
-        });
-      })
-      .catch(() => {
-        return caches.match("/offline/"); // WIP - update template indicating user is offline
-      })
-  );
+  const request = event.request;
+  if (request.method === "GET") {
+    return event.respondWith(
+      fetchAndCache(request)
+        .then(response =>
+          isValidResponse(response) ? response : caches.match(request)
+        )
+        .then(response => (response ? response : caches.match("/offline/")))
+    );
+  }
 });
